6ed436365e156a37f8015ec9085ac66f7c21f9fa
nadyawilliams <nadya@sdsc.edu>
2015-02-25 18:56:26 -0800

    initial

---
+++ src/cloud-scheduler/version.mk
@@ -0,0 +1,11 @@
+PKGROOT	    = /var/www/html
+NAME        = cloud-scheduler
+PKGNAME     = booked
+VERSION     = 2.5.13
+RELEASE     = 0
+
+WWWDIR      = cloud-sched
+DBNAME      = clouddb
+DBUSER      = pragmac
+
+TARBALL_POSTFIX	= zip
---
+++ version.mk
@@ -0,0 +1,6 @@
+ROLLNAME	= cloud-scheduler
+VERSION		= 1.0
+RELEASE		= 0
+COLOR		= blue
+
+REDHAT.ROOT	= $(CURDIR)
