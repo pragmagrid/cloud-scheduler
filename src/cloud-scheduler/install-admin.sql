b508465201e623f418b93cc64400d43687a56df3
nadyawilliams <nadya@sdsc.edu>
2015-02-27 15:14:49 -0800

    automate db and user creation

--- src/cloud-scheduler/install-admin.sql
+++
@@ -1,8 +0,0 @@
-insert into users (fname, lname, email, username, password, salt, timezone, lastlogin, status_id, date_created, language, organization)
-        values ('$DBuser', '$DBuser', '$ADMIN_EMAL', 'admin', SHA(concat('$DBPASSWD', '$SALT')), '$SALT', '$TIMEZONE', '2010-03-26 00:00:00', 1, now(), 'en_us', 'PRAGMA Cloud');
-
-insert into user_groups values (1,1);
-insert into user_groups values (1,2);
-insert into user_groups values (1,3);
-insert into user_groups values (1,4);
-
