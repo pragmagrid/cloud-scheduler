ad68b0e857ce700e1b51b29b9700197d2540c92d
nadyawilliams <nadya@sdsc.edu>
2015-03-05 18:24:48 -0800

    add default setup for 2 sites

--- src/cloud-scheduler/install-data.sql
+++ src/cloud-scheduler/install-data.sql
@@ -1,9 +1,9 @@
 
 delete from resources;
 alter table resources AUTO_INCREMENT = 1;
-insert into resources (`resource_id`, `name`, `location`, `contact_info`, `description`, `notes`, `min_duration`, `min_increment`, `max_duration`, `unit_cost`, `autoassign`, `requires_approval`, `allow_multiday_reservations`, `max_participants`, `min_notice_time`, `max_notice_time`, `image_name`, `legacyid`, `schedule_id`) VALUES
-(1, 'Conference Room 1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, 1, NULL, NULL, NULL, 'resource1.jpg', NULL, 1),
-(2, 'Conference Room 2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 0, 1, NULL, NULL, NULL, 'resource2.jpg', NULL, 1);
+INSERT INTO resources VALUES  
+(1,'SDSC cloud','UCSD/SDSC','admin@address','Rocks 6.2 KVM.\nHosting Virtual clusters and virtual machines',NULL,NULL,NULL,NULL,NULL,1,0,1,NULL,NULL,NULL,'resource1.jpg',1,NULL,NULL,'54f4e13a8e3d1',0,0,NULL,1,NULL,NULL),
+(2,'UF cloud','University of Florida','admin@address','Rocks 6.2 KVM. ENT-enabled.\nHosting virtual clusters and virtual machines',NULL,NULL,NULL,NULL,NULL,1,0,1,7,NULL,NULL,'resource2.jpg',1,NULL,NULL,'54f4e142c44b5',0,0,NULL,1,NULL,NULL);
 
 delete from accessories;
 alter table accessories AUTO_INCREMENT = 1;
@@ -13,11 +13,28 @@ insert into accessories (`accessory_id`, `accessory_name`, `accessory_quantity`)
 (3, 'unlimited accessory', NULL);
 
 truncate table custom_attributes;
-insert into custom_attributes(`custom_attribute_id`,`display_label`,`display_type`,`attribute_category`,`validation_regex`,`is_required`,`possible_values`) VALUES
-(1, 'Test Number', 1, 1, null, false, null),
-(2, 'Test String', 1, 1, null, false, null),
-(3, 'Test Number', 1, 4, null, false, null),
-(4, 'Test String', 1, 4, null, false, null);
+INSERT INTO `custom_attributes` VALUES
+( 1, 'Available CPUs (total)', 5, 4, '', 1, NULL, 1, NULL),
+( 2, 'Available Memory (Gb)',  5, 4, '', 1, NULL, 2, NULL),
+( 3, 'Deployment type',        3, 4, '', 1, 'Rocks KVM, OpenNebula, OpenStack, CloudStack', 3, NULL),
+( 4, 'ENT-enabled',            3, 4, '', 1, 'yes, no', 4, NULL),
+( 5, 'Affiliation',            1, 2, '', 1, NULL, 0, NULL),
+( 6, 'SSH public key',         1, 2, '', 1, NULL, 1, NULL),
+( 7, 'CPU (per host)',         5, 1, '', 1, NULL, 1, NULL),
+( 8, 'Memory (Gb/host)',       5, 1, '', 1, NULL,2,NULL),
+( 9, 'Deployment type',        3, 1, '', 1, 'ROCKS KVM, OpenNebula, OpenStack, CloudStack', 3, NULL),
+(10, 'ENT-enabled',            3, 1, '', 1, 'yes, no', 4, NULL);
+
+truncate table custom_attribute_values;
+INSERT INTO `custom_attribute_values` VALUES
+(1, 1, '16',         1, 4 ),
+(2, 2, '32',         1, 4 ),
+(3, 3, 'Rocks KVM',  1, 4 ),
+(4, 4, 'no',         1, 4 ),
+(5, 1, '32',         2, 4 ),
+(6, 2, '64',         2, 4 ),
+(7, 3, 'Rocks KVM',  2, 4 ),
+(8, 4, 'yes',        2, 4 );
 
 /* user_id resource_id, permission_id, add tuples if adding more users in install-admin.sql */
 truncate table user_resource_permissions;
