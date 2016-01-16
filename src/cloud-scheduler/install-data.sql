13ead37b3c99eb9dc52afafc08184115dd5af5f9
root <root@fiji.rocksclusters.org>
2016-01-16 10:58:24 -0800

    Updated some attribute names for reservation

--- src/cloud-scheduler/install-data.sql
+++ src/cloud-scheduler/install-data.sql
@@ -20,12 +20,11 @@ INSERT INTO `custom_attributes` VALUES
 ( 4, 'ENT-enabled',            3, 4, '', 1, 'yes, no', 4, NULL),
 ( 5, 'Affiliation',            1, 2, '', 1, NULL, 0, NULL),
 ( 6, 'SSH public key',         1, 2, '', 1, NULL, 1, NULL),
-( 7, 'CPU (per host)',         5, 1, '', 1, NULL, 1, NULL),
-( 8, 'Memory (Gb)',       5, 1, '', 1, NULL,2,NULL),
-( 9, 'Deployment type',        3, 1, '', 1, 'ROCKS KVM, OpenNebula, OpenStack, CloudStack', 3, NULL),
-(10, 'ENT-enabled',            3, 1, '', 1, 'yes, no', 4, NULL),
-(11, 'Site hostname',          1, 4, '', 1, NULL, 5, NULL),
-(12, 'VC Name',                3, 1, '', 1, 'docking,lmserver,lmcompute,rocks-sge-ipop', 5, NULL);
+( 7, 'CPUs',         5, 1, '', 1, NULL, 1, NULL),
+( 8, 'Memory (GB)',       5, 1, '', 1, NULL,2,NULL),
+ (9, 'ENT-enabled',            3, 1, '', 1, 'yes, no', 4, NULL),
+(10, 'Site hostname',          1, 4, '', 1, NULL, 5, NULL),
+(11, 'VC Name',                3, 1, '', 1, 'docking,lmserver,lmcompute,rocks-sge-ipop', 5, NULL);
 
 truncate table custom_attribute_values;
 INSERT INTO `custom_attribute_values` VALUES
