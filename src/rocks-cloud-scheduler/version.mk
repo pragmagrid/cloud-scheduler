24d7d8c721dbc75d454e72e43f62e2b69005c9a1
nadyawilliams <nadya@sdsc.edu>
2017-05-19 15:42:00 -0700

    separate scripts and config files

---
+++ src/rocks-cloud-scheduler/version.mk
@@ -0,0 +1,13 @@
+NAME = rocks-cloud-scheduler
+CSNAME = cloud-scheduler
+WWWROOT = /var/www/html/$(CSNAME)
+PKGROOT = /opt/$(CSNAME)
+
+VERSION   = 1.0
+RELEASE = 0
+ 
+DBNAME    = pragma
+DBUSER    = pragmac
+DBSCHEMA  = cs-initdb
+DBDATA    = pragma
+
