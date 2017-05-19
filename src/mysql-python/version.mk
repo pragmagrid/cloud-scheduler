70f409b0841fec0f23ea7875906fee4a87de55ab
nadyawilliams <nadya@sdsc.edu>
2017-05-19 13:52:09 -0700

    add mysql-python

---
+++ src/mysql-python/version.mk
@@ -0,0 +1,10 @@
+PKGROOT     = /opt/python
+NAME        = cloud-scheduler-MySQL-python
+ARCHIVENAME = MySQL-python
+VERSION     = 1.2.5
+RELEASE     = 0
+
+PYTHONVER   = python2.7
+RPM.EXTRAS += "AutoReq: 0"
+
+
