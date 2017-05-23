d81f749953491bbff986fbba001eb0f40a72db34
nadyawilliams <nadya@sdsc.edu>
2017-05-22 18:32:04 -0700

    disable python dependency

--- src/gui/version.mk
+++ src/gui/version.mk
@@ -11,7 +11,11 @@ DBUSER    = pragmac
 DBSCHEMA  = cs-initdb
 DBDATA    = pragma
 
+PERLFILE = $(PKGNAME)/UI/node_modules/node-sass/src/libsass/script/test-leaks.pl
+
 TARBALL_POSTFIX = tar.gz
 
 PY.PATH = /opt/python/bin/python2.7
-RPM.EXTRAS  = %define __os_install_post /usr/lib/rpm/brp-python-bytecompile  $(PY.PATH)
+#RPM.EXTRAS  = %define __os_install_post /usr/lib/rpm/brp-python-bytecompile  $(PY.PATH)
+RPM.EXTRAS = "AutoReq: no"
+
